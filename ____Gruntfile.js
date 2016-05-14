0b42aabfecb58f7b2488abd666aa0f6aadab1476
brazleonardo <leonardok3b@gmail.com>
2016-05-14 15:15:12 -0300

    Banner G1

---
+++ ____Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+	grunt.initConfig({
+		concat: {
+			css: {
+				src: "src/css/*.css",
+				dest: "dist/css/app.css"
+
+			},
+			js: {
+		 		src: "src/js/*.js",
+		 		dest: "dist/js/app.js"
+			}
+		}
+	});
+};
---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+$(function(){
+	 $(".banner").owlCarousel({
+        autoPlay: 5000,
+        navigation : true, 
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+      });
+})
