9c3e3507c7d50253c834c6bc31fe80d6fd175a5c
brazleonardo <leonardok3b@gmail.com>
2016-05-15 16:15:57 -0300

    Ajuste site

--- src/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	 $(".banner").owlCarousel({
-        autoPlay: 5000,
-        navigation : true, 
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-      });
-})
